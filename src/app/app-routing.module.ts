36cc7b4a61fc9997a330f1376ad5a50df05fee07
syvende <paulinasikacka@gmail.com>
2022-05-08 22:15:38 +0200

    4.2 Utworzenie sciezek routingu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { UploadComponent } from './upload/upload.component';
+import { HomeComponent } from './home/home.component';
 
-
+const routes: Routes = [
+  { path: '', pathMatch: 'full', component: HomeComponent },
+  { path: 'upload', component: UploadComponent },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
