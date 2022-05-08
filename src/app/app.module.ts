a3ea80c71c06712bfd82fff9a817afe112af9997
syvende <paulinasikacka@gmail.com>
2022-05-08 21:17:34 +0200

    2.4 Automatyczne dodanie deklaracji komponentow w module aplikacji

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { UploadComponent } from './upload/upload.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    UploadComponent
   ],
   imports: [
     BrowserModule
